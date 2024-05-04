dd2124da1ecf0b0f1b0a9d71c381d3c56198925f
offwavesurfer <h1000100100@outlook.com>
2024-05-04 23:53:50 +0100

    Baisc websocket implementation

--- ClientApp/src/app/components/counter/counter.component.ts
+++ ClientApp/src/app/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-counter-component',
@@ -6,8 +6,65 @@ import { Component } from '@angular/core';
 })
 export class CounterComponent {
   public currentCount = 0;
+  public webSocketConnected: boolean = false;
+
+  private webSocket: WebSocket;
+
+  constructor(@Inject("BASE_URL") baseUrl: string) {
+    this.webSocket = new WebSocket(`${baseUrl}ws`);
+
+    this.webSocket.onerror = (ev: Event) => {
+      this.log("Web Socket error", ev);
+    };
+
+    this.webSocket.onopen = (ev: Event) => {
+      this.log("Web Socket open", ev);
+
+      this.webSocketConnected = true;
+    };
+
+    this.webSocket.onclose = (ev: Event) => {
+      this.log("Web Socket close", ev);
+
+      this.webSocketConnected = false;
+    };
+
+    this.webSocket.onmessage = (ev: Event) => {
+      this.log("Web Socket message", ev);
+    };
+  }
 
   public incrementCounter() {
     this.currentCount++;
+  }
+
+  public test(): void {
+    if (this.webSocketConnected === false)
+      return;
+
+    const obj = {
+      prop: "test",
+      prop1: {
+        x: "test1"
+      },
+      prop2: [
+        "test2",
+        "test3",
+        "test4"
+      ]
+    };
+
+    const json: string = JSON.stringify(obj);
+
+    this.webSocket.send(json);
+  }
+
+  private log(message: string, value: any = null) {
+    if (!value) {
+      console.log(`[${new Date().toISOString()}] ${message}`);
+    }
+    else {
+      console.log(`[${new Date().toISOString()}] ${message}`, value);
+    }
   }
 }
